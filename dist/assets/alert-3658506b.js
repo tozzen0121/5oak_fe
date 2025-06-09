92a579f13ec93e75cc0534b826429c03d3c22629
tozzen0121 <tozzen0121@gmail.com>
2025-06-09 02:33:09 -0400

    update

---
+++ dist/assets/alert-3658506b.js
@@ -0,0 +1,101 @@
+import{bf as z,be as S,a3 as b,T as s,r as i,bi as M,b3 as F,j as e,a0 as a,bn as B,bo as W,$ as c,c as V,G as g,p as l,M as T,cB as o,cU as f,cV as I,d as C}from"./index-b5c3012f.js";import{C as Q}from"./ComponentHeader-94965ad9.js";import{C as H,a as $}from"./ComponentSkeleton-0ecce135.js";import{D as v}from"./DatabaseFilled-e546c80d.js";import{I as m}from"./InfoCircleFilled-a5683b44.js";import{W as p}from"./WarningFilled-a02044af.js";import{B as w}from"./BugFilled-448e0747.js";import"./Skeleton-d9a17e6d.js";function E(n){return z("MuiAlertTitle",n)}S("MuiAlertTitle",["root"]);const k=["className"],q=n=>{const{classes:r}=n;return W({root:["root"]},E,r)},R=b(s,{name:"MuiAlertTitle",slot:"Root",overridesResolver:(n,r)=>r.root})(({theme:n})=>({fontWeight:n.typography.fontWeightMedium,marginTop:-2})),D=i.forwardRef(function(r,t){const h=M({props:r,name:"MuiAlertTitle"}),{className:u}=h,x=F(h,k),j=h,y=q(j);return e.jsx(R,a({gutterBottom:!0,component:"div",ownerState:j,ref:t,className:B(y.root,u)},x))}),d=D;var P={icon:function(r,t){return{tag:"svg",attrs:{viewBox:"64 64 896 896",focusable:"false"},children:[{tag:"path",attrs:{d:"M308 412v268c0 36.78 9.68 71.96 27.8 102.9a205.39 205.39 0 0073.3 73.3A202.68 202.68 0 00512 884c36.78 0 71.96-9.68 102.9-27.8a205.39 205.39 0 0073.3-73.3A202.68 202.68 0 00716 680V412H308zm484 172v96c0 6.5-.22 12.95-.66 19.35C859.94 728.64 908 796.7 908 876a8 8 0 01-8 8h-56a8 8 0 01-8-8c0-44.24-23.94-82.89-59.57-103.7a278.63 278.63 0 01-22.66 49.02 281.39 281.39 0 01-100.45 100.45C611.84 946.07 563.55 960 512 960s-99.84-13.93-141.32-38.23a281.39 281.39 0 01-100.45-100.45 278.63 278.63 0 01-22.66-49.02A119.95 119.95 0 00188 876a8 8 0 01-8 8h-56a8 8 0 01-8-8c0-79.3 48.07-147.36 116.66-176.65A284.12 284.12 0 01232 680v-96H84a8 8 0 01-8-8v-56a8 8 0 018-8h148V412c-76.77 0-139-62.23-139-139a8 8 0 018-8h60a8 8 0 018 8 63 63 0 0063 63h560a63 63 0 0063-63 8 8 0 018-8h60a8 8 0 018 8c0 76.77-62.23 139-139 139v100h148a8 8 0 018 8v56a8 8 0 01-8 8H792zM368 272a8 8 0 01-8 8h-56a8 8 0 01-8-8c0-40.04 8.78-76.75 25.9-108.07a184.57 184.57 0 0174.03-74.03C427.25 72.78 463.96 64 504 64h16c40.04 0 76.75 8.78 108.07 25.9a184.57 184.57 0 0174.03 74.03C719.22 195.25 728 231.96 728 272a8 8 0 01-8 8h-56a8 8 0 01-8-8c0-28.33-5.94-53.15-17.08-73.53a112.56 112.56 0 00-45.39-45.4C573.15 141.95 548.33 136 520 136h-16c-28.33 0-53.15 5.94-73.53 17.08a112.56 112.56 0 00-45.4 45.39C373.95 218.85 368 243.67 368 272z",fill:r}},{tag:"path",attrs:{d:"M308 412v268c0 36.78 9.68 71.96 27.8 102.9a205.39 205.39 0 0073.3 73.3A202.68 202.68 0 00512 884c36.78 0 71.96-9.68 102.9-27.8a205.39 205.39 0 0073.3-73.3A202.68 202.68 0 00716 680V412H308z",fill:t}}]}},name:"bug",theme:"twotone"};const O=P;var U=function(r,t){return i.createElement(c,a({},r,{ref:t,icon:O}))};const L=i.forwardRef(U);var N={icon:function(r,t){return{tag:"svg",attrs:{viewBox:"64 64 896 896",focusable:"false"},children:[{tag:"path",attrs:{d:"M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zm-40 728H184V184h656v656z",fill:r}},{tag:"path",attrs:{d:"M184 840h656V184H184v656zm130-367.8h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H688c6.5 0 10.3 7.4 6.5 12.7l-210.6 292a31.8 31.8 0 01-51.7 0L307.5 484.9c-3.8-5.3 0-12.7 6.5-12.7z",fill:t}},{tag:"path",attrs:{d:"M432.2 657.7a31.8 31.8 0 0051.7 0l210.6-292c3.8-5.3 0-12.7-6.5-12.7h-46.9c-10.3 0-19.9 5-25.9 13.3L458 584.3l-71.2-98.8c-6-8.4-15.7-13.3-25.9-13.3H314c-6.5 0-10.3 7.4-6.5 12.7l124.7 172.8z",fill:r}}]}},name:"check-square",theme:"twotone"};const _=N;var G=function(r,t){return i.createElement(c,a({},r,{ref:t,icon:_}))};const J=i.forwardRef(G);var K={icon:function(r,t){return{tag:"svg",attrs:{viewBox:"64 64 896 896",focusable:"false"},children:[{tag:"path",attrs:{d:"M232 616h560V408H232v208zm112-144c22.1 0 40 17.9 40 40s-17.9 40-40 40-40-17.9-40-40 17.9-40 40-40zM232 888h560V680H232v208zm112-144c22.1 0 40 17.9 40 40s-17.9 40-40 40-40-17.9-40-40 17.9-40 40-40zM232 344h560V136H232v208zm112-144c22.1 0 40 17.9 40 40s-17.9 40-40 40-40-17.9-40-40 17.9-40 40-40z",fill:t}},{tag:"path",attrs:{d:"M304 512a40 40 0 1080 0 40 40 0 10-80 0zm0 272a40 40 0 1080 0 40 40 0 10-80 0zm0-544a40 40 0 1080 0 40 40 0 10-80 0z",fill:r}},{tag:"path",attrs:{d:"M832 64H192c-17.7 0-32 14.3-32 32v832c0 17.7 14.3 32 32 32h640c17.7 0 32-14.3 32-32V96c0-17.7-14.3-32-32-32zm-40 824H232V680h560v208zm0-272H232V408h560v208zm0-272H232V136h560v208z",fill:r}}]}},name:"database",theme:"twotone"};const X=K;var Y=function(r,t){return i.createElement(c,a({},r,{ref:t,icon:X}))};const Z=i.forwardRef(Y);var ee={icon:function(r,t){return{tag:"svg",attrs:{viewBox:"64 64 896 896",focusable:"false"},children:[{tag:"path",attrs:{d:"M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z",fill:r}},{tag:"path",attrs:{d:"M512 140c-205.4 0-372 166.6-372 372s166.6 372 372 372 372-166.6 372-372-166.6-372-372-372zm32 588c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V456c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272zm-32-344a48.01 48.01 0 010-96 48.01 48.01 0 010 96z",fill:t}},{tag:"path",attrs:{d:"M464 336a48 48 0 1096 0 48 48 0 10-96 0zm72 112h-48c-4.4 0-8 3.6-8 8v272c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V456c0-4.4-3.6-8-8-8z",fill:r}}]}},name:"info-circle",theme:"twotone"};const re=ee;var te=function(r,t){return i.createElement(c,a({},r,{ref:t,icon:re}))};const ne=i.forwardRef(te);var oe={icon:{tag:"svg",attrs:{viewBox:"64 64 896 896",focusable:"false"},children:[{tag:"path",attrs:{d:"M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 708c-22.1 0-40-17.9-40-40s17.9-40 40-40 40 17.9 40 40-17.9 40-40 40zm62.9-219.5a48.3 48.3 0 00-30.9 44.8V620c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8v-21.5c0-23.1 6.7-45.9 19.9-64.9 12.9-18.6 30.9-32.8 52.1-40.9 34-13.1 56-41.6 56-72.7 0-44.1-43.1-80-96-80s-96 35.9-96 80v7.6c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V420c0-39.3 17.2-76 48.4-103.3C430.4 290.4 470 276 512 276s81.6 14.5 111.6 40.7C654.8 344 672 380.7 672 420c0 57.8-38.1 109.8-97.1 132.5z"}}]},name:"question-circle",theme:"filled"};const ie=oe;var ae=function(r,t){return i.createElement(c,a({},r,{ref:t,icon:ie}))};const A=i.forwardRef(ae);var ce={icon:{tag:"svg",attrs:{viewBox:"64 64 896 896",focusable:"false"},children:[{tag:"path",attrs:{d:"M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"}},{tag:"path",attrs:{d:"M623.6 316.7C593.6 290.4 554 276 512 276s-81.6 14.5-111.6 40.7C369.2 344 352 380.7 352 420v7.6c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V420c0-44.1 43.1-80 96-80s96 35.9 96 80c0 31.1-22 59.6-56.1 72.7-21.2 8.1-39.2 22.3-52.1 40.9-13.1 19-19.9 41.8-19.9 64.9V620c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8v-22.7a48.3 48.3 0 0130.9-44.8c59-22.7 97.1-74.7 97.1-132.5.1-39.3-17.1-76-48.3-103.3zM472 732a40 40 0 1080 0 40 40 0 10-80 0z"}}]},name:"question-circle",theme:"outlined"};const le=ce;var se=function(r,t){return i.createElement(c,a({},r,{ref:t,icon:le}))};const de=i.forwardRef(se);var he={icon:function(r,t){return{tag:"svg",attrs:{viewBox:"64 64 896 896",focusable:"false"},children:[{tag:"path",attrs:{d:"M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z",fill:r}},{tag:"path",attrs:{d:"M512 140c-205.4 0-372 166.6-372 372s166.6 372 372 372 372-166.6 372-372-166.6-372-372-372zm0 632c-22.1 0-40-17.9-40-40s17.9-40 40-40 40 17.9 40 40-17.9 40-40 40zm62.9-219.5a48.3 48.3 0 00-30.9 44.8V620c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8v-21.5c0-23.1 6.7-45.9 19.9-64.9 12.9-18.6 30.9-32.8 52.1-40.9 34-13.1 56-41.6 56-72.7 0-44.1-43.1-80-96-80s-96 35.9-96 80v7.6c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V420c0-39.3 17.2-76 48.4-103.3C430.4 290.4 470 276 512 276s81.6 14.5 111.6 40.7C654.8 344 672 380.7 672 420c0 57.8-38.1 109.8-97.1 132.5z",fill:t}},{tag:"path",attrs:{d:"M472 732a40 40 0 1080 0 40 40 0 10-80 0zm151.6-415.3C593.6 290.5 554 276 512 276s-81.6 14.4-111.6 40.7C369.2 344 352 380.7 352 420v7.6c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V420c0-44.1 43.1-80 96-80s96 35.9 96 80c0 31.1-22 59.6-56 72.7-21.2 8.1-39.2 22.3-52.1 40.9-13.2 19-19.9 41.8-19.9 64.9V620c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8v-22.7a48.3 48.3 0 0130.9-44.8c59-22.7 97.1-74.7 97.1-132.5 0-39.3-17.2-76-48.4-103.3z",fill:r}}]}},name:"question-circle",theme:"twotone"};const Te=he;var ue=function(r,t){return i.createElement(c,a({},r,{ref:t,icon:Te}))};const xe=i.forwardRef(ue);var ge={icon:function(r,t){return{tag:"svg",attrs:{viewBox:"64 64 896 896",focusable:"false"},children:[{tag:"path",attrs:{d:"M955.7 856l-416-720c-6.2-10.7-16.9-16-27.7-16s-21.6 5.3-27.7 16l-416 720C56 877.4 71.4 904 96 904h832c24.6 0 40-26.6 27.7-48zm-783.5-27.9L512 239.9l339.8 588.2H172.2z",fill:r}},{tag:"path",attrs:{d:"M172.2 828.1h679.6L512 239.9 172.2 828.1zM560 720a48.01 48.01 0 01-96 0 48.01 48.01 0 0196 0zm-16-304v184c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V416c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8z",fill:t}},{tag:"path",attrs:{d:"M464 720a48 48 0 1096 0 48 48 0 10-96 0zm16-304v184c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V416c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8z",fill:r}}]}},name:"warning",theme:"twotone"};const fe=ge;var ve=function(r,t){return i.createElement(c,a({},r,{ref:t,icon:fe}))};const me=i.forwardRef(ve),be=()=>{const n=V(),r=`<Alert color="primary" icon={<QuestionCircleFilled />}>
+  Primary Text
+</Alert>
+<Alert color="secondary" icon={<DatabaseFilled />}>
+  Secondary Text
+</Alert>
+<Alert color="success" icon={<CheckSquareFilled />}>
+  Success Text
+</Alert>
+<Alert color="warning" icon={<WarningFilled />}>
+  Warning Text
+</Alert>
+<Alert color="info" icon={<InfoCircleFilled />}>
+  Info Text
+</Alert>
+<Alert color="error" icon={<BugFilled />}>
+  Error Text
+</Alert>`,t=`<Alert variant="border" color="success" onClose={() => {}}>
+  Success Text
+</Alert>
+<Alert
+  variant="border"
+  color="warning"
+  icon={<WarningOutlined />}
+  action={
+    <Button color="warning" size="small">
+      Undo
+    </Button>
+  }
+>
+  Warning Text
+</Alert>
+<Alert
+  variant="border"
+  color="primary"
+  icon={<QuestionCircleOutlined />}
+  action={
+    <Button variant="contained" size="small">
+      Continue
+    </Button>
+  }
+>
+  Primary Text
+</Alert>`,h=`<Alert color="primary" variant="filled" icon={<QuestionCircleFilled />}>
+  Primary Text
+</Alert>
+<Alert color="secondary" variant="filled" icon={<DatabaseFilled />}>
+  Secondary Text
+</Alert>
+<Alert color="success" variant="filled" icon={<CheckSquareFilled />}>
+  Success Text
+</Alert>
+<Alert color="warning" variant="filled" icon={<WarningFilled />}>
+  Warning Text
+</Alert>
+<Alert color="info" variant="filled" icon={<InfoCircleFilled />}>
+  Info Text
+</Alert>
+<Alert color="error" variant="filled" icon={<BugFilled />}>
+  Error Text
+</Alert>`,u=`<Alert color="primary" variant="border" icon={<QuestionCircleFilled />}>
+  <AlertTitle>Primary Text</AlertTitle>
+  <Typography variant="h6"> This is an primary alert.</Typography>
+</Alert>
+<Alert color="secondary" variant="border" icon={<DatabaseFilled />}>
+  <AlertTitle>Secondary Text</AlertTitle>
+  <Typography variant="h6"> This is an secondary alert.</Typography>
+</Alert>
+<Alert color="success" variant="border" icon={<CheckSquareFilled />}>
+  <AlertTitle>Success Text</AlertTitle>
+  <Typography variant="h6"> This is an success alert.</Typography>
+</Alert>
+<Alert color="warning" variant="border" icon={<WarningFilled />}>
+  <AlertTitle>Warning Text</AlertTitle>
+  <Typography variant="h6"> This is an warning alert.</Typography>
+</Alert>
+<Alert color="info" variant="border" icon={<InfoCircleFilled />}>
+  <AlertTitle>Info Text</AlertTitle>
+  <Typography variant="h6"> This is an info alert.</Typography>
+</Alert>
+<Alert color="error" variant="border" icon={<BugFilled />}>
+  <AlertTitle>Error Text</AlertTitle>
+  <Typography variant="h6"> This is an error alert.</Typography>
+</Alert>`,x=`<Alert color="primary" variant="outlined" icon={<QuestionCircleTwoTone />}>
+  Primary Text
+</Alert>
+<Alert color="secondary" variant="outlined" icon={<DatabaseTwoTone twoToneColor={theme.palette.secondary.main} />}>
+  Secondary Text
+</Alert>
+<Alert color="success" variant="outlined" icon={<CheckSquareTwoTone twoToneColor={theme.palette.success.main} />}>
+  Success Text
+</Alert>
+<Alert color="warning" variant="outlined" icon={<WarningTwoTone twoToneColor={theme.palette.warning.main} />}>
+  Warning Text
+</Alert>
+<Alert color="info" variant="outlined" icon={<InfoCircleTwoTone twoToneColor={theme.palette.info.main} />}>
+  Info Text
+</Alert>
+<Alert color="error" variant="outlined" icon={<BugTwoTone twoToneColor={theme.palette.error.main} />}>
+  Error Text
+</Alert>`;return e.jsxs(H,{children:[e.jsx(Q,{title:"Alert",caption:"An alert displays a short, important message in a way that attracts the user's attention without interrupting the user's task.",directory:"src/pages/components-overview/alert",link:"https://mui.com/material-ui/react-alert/"}),e.jsx($,{children:e.jsxs(g,{container:!0,spacing:3,children:[e.jsx(g,{item:!0,xs:12,md:6,children:e.jsxs(l,{spacing:3,children:[e.jsx(T,{title:"Basic",codeHighlight:!0,codeString:r,children:e.jsxs(l,{sx:{width:"100%"},spacing:2,children:[e.jsx(o,{color:"primary",icon:e.jsx(A,{}),children:"Primary Text"}),e.jsx(o,{color:"secondary",icon:e.jsx(v,{}),children:"Secondary Text"}),e.jsx(o,{color:"success",icon:e.jsx(f,{}),children:"Success Text"}),e.jsx(o,{color:"warning",icon:e.jsx(p,{}),children:"Warning Text"}),e.jsx(o,{color:"info",icon:e.jsx(m,{}),children:"Info Text"}),e.jsx(o,{color:"error",icon:e.jsx(w,{}),children:"Error Text"})]})}),e.jsx(T,{title:"Actions",codeString:t,children:e.jsxs(l,{sx:{width:"100%"},spacing:2,children:[e.jsx(o,{variant:"border",color:"success",onClose:()=>{},sx:{"& .MuiIconButton-root:focus-visible":{outline:`2px solid ${n.palette.success.dark}`,outlineOffset:2}},children:"Success Text"}),e.jsx(o,{variant:"border",color:"warning",icon:e.jsx(I,{}),action:e.jsx(C,{color:"warning",size:"small",children:"Undo"}),children:"Warning Text"}),e.jsx(o,{variant:"border",color:"primary",icon:e.jsx(de,{}),action:e.jsx(C,{variant:"contained",size:"small",children:"Continue"}),children:"Primary Text"})]})}),e.jsx(T,{title:"Filled",codeString:h,children:e.jsxs(l,{sx:{width:"100%"},spacing:2,children:[e.jsx(o,{color:"primary",variant:"filled",icon:e.jsx(A,{}),children:"Primary Text"}),e.jsx(o,{color:"secondary",variant:"filled",icon:e.jsx(v,{}),children:"Secondary Text"}),e.jsx(o,{color:"success",variant:"filled",icon:e.jsx(f,{}),children:"Success Text"}),e.jsx(o,{color:"warning",variant:"filled",icon:e.jsx(p,{}),children:"Warning Text"}),e.jsx(o,{color:"info",variant:"filled",icon:e.jsx(m,{}),children:"Info Text"}),e.jsx(o,{color:"error",variant:"filled",icon:e.jsx(w,{}),children:"Error Text"})]})})]})}),e.jsx(g,{item:!0,xs:12,md:6,children:e.jsxs(l,{spacing:3,children:[e.jsx(T,{title:"Description",codeString:u,children:e.jsxs(l,{sx:{width:"100%"},spacing:2,children:[e.jsxs(o,{color:"primary",variant:"border",icon:e.jsx(A,{}),children:[e.jsx(d,{children:"Primary Text"}),e.jsx(s,{variant:"h6",children:" This is an primary alert."})]}),e.jsxs(o,{color:"secondary",variant:"border",icon:e.jsx(v,{}),children:[e.jsx(d,{children:"Secondary Text"}),e.jsx(s,{variant:"h6",children:" This is an secondary alert."})]}),e.jsxs(o,{color:"success",variant:"border",icon:e.jsx(f,{}),children:[e.jsx(d,{children:"Success Text"}),e.jsx(s,{variant:"h6",children:" This is an success alert."})]}),e.jsxs(o,{color:"warning",variant:"border",icon:e.jsx(p,{}),children:[e.jsx(d,{children:"Warning Text"}),e.jsx(s,{variant:"h6",children:" This is an warning alert."})]}),e.jsxs(o,{color:"info",variant:"border",icon:e.jsx(m,{}),children:[e.jsx(d,{children:"Info Text"}),e.jsx(s,{variant:"h6",children:" This is an info alert."})]}),e.jsxs(o,{color:"error",variant:"border",icon:e.jsx(w,{}),children:[e.jsx(d,{children:"Error Text"}),e.jsx(s,{variant:"h6",children:" This is an error alert."})]})]})}),e.jsx(T,{title:"Outlined",codeString:x,children:e.jsxs(l,{sx:{width:"100%"},spacing:2,children:[e.jsx(o,{color:"primary",variant:"outlined",icon:e.jsx(xe,{}),children:"Primary Text"}),e.jsx(o,{color:"secondary",variant:"outlined",icon:e.jsx(Z,{twoToneColor:n.palette.secondary.main}),children:"Secondary Text"}),e.jsx(o,{color:"success",variant:"outlined",icon:e.jsx(J,{twoToneColor:n.palette.success.main}),children:"Success Text"}),e.jsx(o,{color:"warning",variant:"outlined",icon:e.jsx(me,{twoToneColor:n.palette.warning.main}),children:"Warning Text"}),e.jsx(o,{color:"info",variant:"outlined",icon:e.jsx(ne,{twoToneColor:n.palette.info.main}),children:"Info Text"}),e.jsx(o,{color:"error",variant:"outlined",icon:e.jsx(L,{twoToneColor:n.palette.error.main}),children:"Error Text"})]})})]})})]})})]})};export{be as default};
---
+++ dist/assets/useScriptRef-ebfe97ac.js
@@ -0,0 +1,1 @@
+import{r}from"./index-b5c3012f.js";const s=()=>{const e=r.useRef(!0);return r.useEffect(()=>()=>{e.current=!1},[]),e};export{s as u};
